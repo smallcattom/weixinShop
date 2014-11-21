813240f9f75df2e17c88fe36614d9fc0f5936413
sae-web@sina.com.cn <sae-web@sina.com.cn@4126e22b-221f-483b-af35-ce53868d558b>
2014-11-21 23:25:31 +0000

    auto versioning

---
+++ 1/index.py
@@ -0,0 +1,10 @@
+# -*- coding:utf-8 -*-
+import sae
+
+def app(environ, start_response):
+    status = '200 OK'
+    response_headers = [('Content-type', 'text/plain')]
+    start_response(status, response_headers)
+    return ['hello,中文']
+
+application = sae.create_wsgi_app(app)
